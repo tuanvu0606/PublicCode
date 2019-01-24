6f894300301403fd38c18c4e5da616709b084db4
tuanvu0606 <tuanvu0695@gmail.com>
2019-01-24 12:04:13 +0700

    Update html_parsing.rb

--- html_parsing.rb
+++ html_parsing.rb
@@ -6,10 +6,9 @@ require 'open-uri'
 PAGE_URL = 'https://s3-ap-southeast-1.amazonaws.com/yoose-tmp/Banner_for_v4/TheCoffeeHouse_1/TheCoffeeHouse_creative_3.html'
 page = Nokogiri::HTML(open(PAGE_URL))
    
-puts page.css("div")[0].name   # => title
-puts page.css("div")[0].text   # => My webpage
+puts page.css("div")
 
 f = File.new('out.html', 'w')
-f.write(page.css("div")[0].text)
+f.write(page.css("div"))
 f.write("Hello Foo!\n")
 f.close    
