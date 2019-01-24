0dbed19ad464cdc9bdfb3b62c2783398a37c9199
tuanvu0606 <tuanvu0695@gmail.com>
2019-01-24 11:57:39 +0700

    Update html_parsing.rb

--- html_parsing.rb
+++ html_parsing.rb
@@ -5,6 +5,7 @@ require 'open-uri'
    
 html_data = open('https://s3-ap-southeast-1.amazonaws.com/yoose-tmp/Banner_for_v4/TheCoffeeHouse_1/TheCoffeeHouse_creative_3.html').read
 nokogiri_object = Nokogiri::HTML(html_data)
+puts nokogiri_object
 tagcloud_elements = nokogiri_object.xpath("//ul[@class='banner']/li/a")
 
 tagcloud_elements.each do |tagcloud_element|
