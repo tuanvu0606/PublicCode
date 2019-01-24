9908e6edb05a6dfa871d2fca888d9186ce6e1743
tuanvu0606 <tuanvu0695@gmail.com>
2019-01-24 13:19:22 +0700

    Update html_parsing.rb

--- html_parsing.rb
+++ html_parsing.rb
@@ -3,14 +3,17 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
    
-PAGE_URL = 'https://s3-ap-southeast-1.amazonaws.com/yoose-tmp/Banner_for_v4/TheCoffeeHouse_1/TheCoffeeHouse_creative_3.html'
+PAGE_URL = 'https://s3-ap-southeast-1.amazonaws.com/yoose-tmp/Banner_for_v4/TheCoffeeHouse_1/TheCoffeeHouse_creative_3+-+Copy.html'
 page = Nokogiri::HTML(open(PAGE_URL))
-   
+
+h1  = page.at_css "h1"
+
+h1.content = "Snap, Crackle & Pop"
+
 puts page.css("div")
 
 output = page.css("div").to_s
 
 f = File.new('out.html', 'w')
-f.write(page.css("div"))
-f.write("div")
+f.write(page.to_html)
 f.close    
